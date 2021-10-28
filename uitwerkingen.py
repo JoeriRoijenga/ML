a865b4eb6d3079da7c08cc544153127e64d0d264
JoeriRoijenga <roijenga.joeri@gmail.com>
2021-10-28 14:54:43 +0200

    assignment 1a is done

--- uitwerkingen.py
+++ uitwerkingen.py
@@ -10,9 +10,9 @@ def plot_image(img, label):
     # Maak gebruik van plt.cm.binary voor de cmap-parameter van plt.imgshow.
 
     # YOUR CODE HERE
-
-    pass
-
+    plt.imshow(img)
+    plt.xlabel(label)
+    plt.show()
 
 # OPGAVE 1b
 def scale_data(X):
